4b41baada7995358eccf49b55248bb80d3bfc332
Laura Serrano _ Keajse <lajusega@gmail.com>
2020-10-04 16:59:56 -0500

    Actualización

---
+++ AM2_M1_Adaptadores_ListView/app/src/main/java/com/example/am2_m1_adaptadores_listview/ModeloDos.java
@@ -0,0 +1,33 @@
+package com.example.am2_m1_adaptadores_listview;
+
+public class ModeloDos {
+
+    private String etnombres;
+    private String ettelefono;
+
+
+    public ModeloDos(String etnombres, String ettelefono){
+        this.etnombres = etnombres;
+        this.ettelefono = ettelefono;
+    }
+
+
+    public String getEtnombres() {
+        return etnombres;
+    }
+
+    public void setEtnombres(String etnombres) {
+        this.etnombres = etnombres;
+    }
+
+    public String getEttelefono() {
+        return ettelefono;
+    }
+
+    public void setEttelefono(String ettelefono) {
+        this.ettelefono = ettelefono;
+    }
+
+
+
+}
